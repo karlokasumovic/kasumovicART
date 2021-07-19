f8499404384803114a1d89e76c96961821e5ca80
karlokasumovic <karlo.kasumovic@gmail.com>
2021-07-19 21:07:52 +0200

    image grid changed so that it is responsive

--- my-app/src/components/ImageGrid/ImageGrid.js
+++ my-app/src/components/ImageGrid/ImageGrid.js
@@ -7,7 +7,7 @@ const ImageGrid = ({ setSelectedImg }) => {
         <div className="img-grid">
             {docs && docs.map(doc => (
                 <div className="img-wrap" key={doc.id} onClick={() => setSelectedImg(doc.url)}>
-                    <img className="img-img" src={doc.url} alt="uploaded from firebase"></img>
+                    {doc.id && <img className="img-img" src={doc.url} alt="uploaded from firebase"></img>}
                 </div>
             ))}
         </div>
