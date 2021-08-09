2d72f5b4b2808541cf5b9406ccf4b73213530c9f
karlokasumovic <karlo.kasumovic@gmail.com>
2021-08-09 16:18:21 +0200

    changed route for paintings

--- my-app/src/screens/OtherArtScreen/OtherArtScreen.js
+++ my-app/src/screens/OtherArtScreen/OtherArtScreen.js
@@ -6,9 +6,10 @@ const OtherArtScreen = () => {
                 <img className="img-other-art" src="/images-test/mini-collage.jpg" alt="Grad"></img>
                 <img className="img-other-art" src="/images-test/mini-paintings.jpg" alt="Grad"></img>
 
+
+                <div className="text-box-1"><p className="text-box-1-heading">Mini Collage</p> <p className="text-box-1-text">See different miniature collage creations</p></div>
+                <div className="text-box-2"><p className="text-box-2-heading">Mini Painting</p> <p className="text-box-2-text">See different miniature painting creations</p></div>
             </div>
-            <div className="text-box-1"><p className="text-box-1-heading">Mini Collage</p> <p className="text-box-1-text">See different miniature collage creations</p></div>
-            <div className="text-box-2"><p className="text-box-2-heading">Mini Painting</p> <p className="text-box-2-text">See different miniature painting creations</p></div>
         </>
     )
 }
