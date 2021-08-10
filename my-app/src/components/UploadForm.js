fef672397bc59d3f5e59dd204d1ad987c470d8c8
karlokasumovic <karlo.kasumovic@gmail.com>
2021-08-10 18:41:06 +0200

    created a complete upload form for paintings

--- my-app/src/components/UploadForm.js
+++ my-app/src/components/UploadForm.js
@@ -9,17 +9,43 @@ const UploadForm = () => {
 
     const changeHandler = (e) => {
         let selected = e.target.files[0];
+
+        const form = document.querySelector('.input-form');
+        const inputForm1 = form.querySelector('#painting-name');
+        const selectedName = inputForm1.value
+
+        const inputForm2 = form.querySelector('#painting-size');
+        const selectedSize = inputForm2.value
+
+        const inputForm3 = form.querySelector('#painting-price');
+        const selectedPrice = inputForm3.value
+
+        let finalselected = selected
+        finalselected.paintingName = selectedName;
+        finalselected.paintingSize = selectedSize;
+        finalselected.paintingPrice = selectedPrice;
         if (selected && types.includes(selected.type)) {
             setFile(selected);
             setError('')
         } else {
-            setFile(selected); /*change this line to setFile(null) after*/
+            console.log(finalselected)
+            console.log(selectedName)
+            setFile(finalselected); /*change this line to setFile(null) after*/
             setError("Please select an image file(png or jpeg)")
         }
     }
 
     return (
-        <form>
+        <form className="input-form">
+            <label htmlFor="painting-name">Painting name:</label>
+            <input type="text" id="painting-name" name="painting-name"></input>
+            <br></br> <br></br>
+            <label htmlFor="painting-size">Painting size:</label>
+            <input type="text" id="painting-size" name="painting-size"></input>
+            <br></br> <br></br>
+            <label htmlFor="painting-price">Painting price:</label>
+            <input type="text" id="painting-price" name="painting-price"></input>
+            <br></br> <br></br>
             <input type="file" onChange={changeHandler}></input>
             <div className="output">
                 {error && <div className="error">{error}</div>}
